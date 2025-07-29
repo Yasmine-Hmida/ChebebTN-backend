68c83473db502913385abb79ed3af68668f15fcd
Yasmine-Hmida <hmidayasmiine@gmail.com>
2025-07-29 11:32:09 +0100

    Complete createJob

---
+++ controllers/jobController.js
@@ -0,0 +1,30 @@
+// CRUD for Job.js
+
+// Importer jobSchema
+const Job = require("../models/Job")
+
+// Create a new Job
+exports.createJob = async (req , res) => {
+    const {title , company , description , salary , location , jobType , 
+           status , experienceLevel , skills , applicationDeadline} = req.body; // Vérification des données
+    try{
+        const newJob = new Job({ // Création du Job
+            title,
+            company,
+            description,
+            salary,
+            location,
+            jobType,
+            status,
+            experienceLevel,
+            skills,
+            applicationDeadline
+        });
+
+        const savedJob = await newJob.save();
+        res.status(201).json(savedJob);
+    }
+    catch(err){
+        res.status(500).json({message: err.message });
+    }
+}
---
+++ routes/jobRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const jobController = require("../controllers/jobController");
+const router = express.Router();
+
+// Route to create a new Job
+router.post('./jobs', jobController.createJob);
+
+module.exports = router;
