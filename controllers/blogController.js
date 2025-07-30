70b61900713dbc3554e8a1ed8f27d83882a47372
Yasmine-Hmida <hmidayasmiine@gmail.com>
2025-07-30 10:13:00 +0100

    CRUD of Blog Done

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -16,3 +16,61 @@ exports.createBlog = async (req , res) => {
         res.status(500).json({message: err.message});
     }
 };
+
+exports.getAllBlogs = async (req , res) => {
+    try{
+        const blogs = await Blog.find();
+        if(!blogs){
+            return res.status(404).json({message: "Blogs not found!"});
+        }
+        res.status(200).json(blogs);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+exports.getBlogById = async (req , res) => {
+    try{
+        const blog = await Blog.findById(req.params.id);
+        if(!blog){
+            return res.status(404).json({message: "Blog not found!"});
+        }
+        res.status(200).json(blog);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+exports.deleteBlog = async (req, res) => {
+    try{
+        const blog = await Blog.findByIdAndDelete(req.params.id);
+        if(!blog){
+            return res.status(404).json({message: "Blog not found!"});
+        }
+        res.status(200).json({message: "Blog deleted successfully!"});
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+exports.updateBlog = async (req, res) => {
+    const {title, description} = req.body;
+    try{
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            req.params.id,
+            {title , description},
+            {new: true}
+        );
+
+        if(!updatedBlog){
+            return res.status(404).json({message: "Blog not found!"});
+        }
+        res.status(200).json(updatedBlog);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,4 +5,12 @@ const router = express.Router();
 // Route to create a new Blog
 router.post("/blogs" , blogController.createBlog);
 
+router.get("/blogs", blogController.getAllBlogs);
+
+router.get("/blogs/:id" , blogController.getBlogById);
+
+router.delete("/blogs/:id" , blogController.deleteBlog);
+
+router.put("/blogs/:id" , blogController.updateBlog);
+
 module.exports = router;
